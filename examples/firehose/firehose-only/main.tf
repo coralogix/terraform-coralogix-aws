0978575ab10f65b093bfcd9110f61281cf29be42
Amit Mazor <amitmazor37@gmail.com>
2022-06-13 18:55:50 +0300

    fix firehose exmaples module path

--- examples/firehose/firehose-only/main.tf
+++ examples/firehose/firehose-only/main.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 module "cloudwatch_firehose_coralogix" {
-  source                           = "../../modules/firehose"
+  source                           = "../../../modules/firehose"
   firehose_stream                  = var.coralogix_firehose_stream_name
   endpoint_url                     = var.coralogix_endpoint_url
   privatekey                       = var.coralogix_privatekey
