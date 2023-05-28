53189d818dda6e30c2f0cb24b57d46cd8ed9a015
Avivsc-corl <127672158+Avivsc-corl@users.noreply.github.com>
2023-05-28 17:13:34 +0300

    Create outputs.tf s3-sns.tf variables.tf
    Create outputs.tf s3-sns.tf variables.tf to examples/s3-sns

---
+++ examples/s3-sns/s3-sns.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+}
+
+module "s3-sns" {
+  source = "coralogix/aws/coralogix//modules/s3-sns"
+
+  coralogix_region   = "Europe"
+  private_key        = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXX"
+  ssm_enable         = "false"
+  layer_arn          = "<your layer arn>"
+  application_name   = "s3-sns"
+  subsystem_name     = "logs"
+  sns_topic_name     = "test-sns-topic-name"
+  s3_bucket_name     = "test-bucket-name"
+}
