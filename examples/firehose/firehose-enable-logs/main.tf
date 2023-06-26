29da7b941b9f51642b509979545090237ef72aff
Avivsc-corl <127672158+Avivsc-corl@users.noreply.github.com>
2023-06-26 16:11:17 +0300

    Create main.tf
    Add firehose log enable

---
+++ examples/firehose/firehose-enable-logs/main.tf
@@ -0,0 +1,9 @@
+module "cloudwatch_firehose_coralogix" {
+  source                         = "github.com/coralogix/terraform-coralogix-aws//modules/firehose"
+  firehose_stream                = var.coralogix_firehose_stream_name
+  logs_enable                    = true
+  private_key                    = var.private_key
+  coralogix_region               = var.coralogix_region
+  integration_type_logs          = "Default"
+  source_type_logs               = "DirectPut"
+}
