b89bf80b023e6d9984929cd70a47d069e190cf48
Avivsc-corl <127672158+Avivsc-corl@users.noreply.github.com>
2023-06-26 16:08:02 +0300

    Update variables.tf
    Update variables to enable firehose logs

--- modules/firehose/variables.tf
+++ modules/firehose/variables.tf
@@ -3,13 +3,19 @@ variable "firehose_stream" {
   type        = string
 }
 
-variable "privatekey" {
+variable "private_key" {
   description = "Coralogix account private key"
+  type        = string
   sensitive   = true
 }
 
 variable "coralogix_region" {
   description = "Coralogix account region: us, singapore, ireland, india, stockholm [in lower-case letters]"
+  type        = string
+  validation {
+    condition     = contains(["ireland", "stockholm", "india", "singapore", "us"], var.coralogix_region)
+    error_message = "The coralogix region must be one of these values: [ireland, stockholm, india, singapore, us]."
+  }
 }
 
 variable "include_metric_stream_namespaces" {
@@ -40,16 +46,22 @@ variable "output_format" {
   default     = "opentelemetry0.7"
 }
 
-variable "integration_type" {
+variable "integration_type_metrics" {
   description = "The integration type of the firehose delivery stream: 'CloudWatch_Metrics_JSON' or 'CloudWatch_Metrics_OpenTelemetry070'"
   type        = string
   default     = "CloudWatch_Metrics_OpenTelemetry070"
 }
 
 variable "application_name" {
-  description = "The application name of the metrics"
+  description = "The name of your application in Coralogix"
   type        = string
   default     = null
+}
+
+variable "subsystem_name" {
+  description = "The subsystem name of your application in Coralogix"
+  type        = string
+  default     = ""
 }
 
 variable "user_supplied_tags" {
@@ -68,4 +80,41 @@ variable "coralogix_firehose_custom_endpoint" {
   description = "Custom endpoint for Coralogix firehose integration endpoint (https://firehose-ingress.private.coralogix.net:8443/firehose)"
   type        = string
   default     = null
+}
+
+variable "logs_enable" {
+  description = "Enble sending logs to Coralogix"
+  type        = bool
+  default     = false
+}
+
+variable "metric_enable" {
+  description = "The source_type of kinesis firehose: KinesisStreamAsSource or DirectPut"
+  type        = bool
+  default     = false
+}
+
+#for logs:
+variable "source_type_logs" {
+  description = "The source_type of kinesis firehose: KinesisStreamAsSource or DirectPut"
+  type        = string
+  default     = "DirectPut"
+}
+
+variable "kinesis_stream_arn" {
+  description = "The kinesis stream name for the logs - used in kinesis stream as a source"
+  type        = string
+  default     = ""
+}
+
+variable "integration_type_logs" {
+  description = "The integration type of the firehose delivery stream: 'CloudWatch_JSON', 'WAF', 'CloudWatch_CloudTrail', 'EksFargate', 'Default', 'RawText'"
+  type        = string
+  default     = "Default"
+}
+
+variable "dynamic_metadata_logs" {
+  description = "Dynamic values search for specific fields in the logs to populate the fields"
+  type        = bool
+  default     = false
 }
