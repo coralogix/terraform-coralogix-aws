a5bb98ac98cbc5200ba5ae30fc53a7f11338deab
guyrenny <guy.renny@coralogix.com>
2023-07-24 09:53:24 +0300

    remove commants from main.tf

--- modules/resource-metadata/main.tf
+++ modules/resource-metadata/main.tf
@@ -176,26 +176,6 @@ module "lambdaSSM" {
   tags = merge(var.tags, local.tags)
 }
 
-# resource "aws_cloudwatch_event_rule" "profile_generator_lambda_event_rule" {
-#   name = "profile-generator-lambda-event-rule"
-#   description = "retry scheduled every 1 min"
-#   schedule_expression = "rate(1 minutes)"
-# }
-
-# resource "aws_cloudwatch_event_target" "profile_generator_lambda_target" {
-#   arn = module.lambda.lambda_function_arn
-#   rule = aws_cloudwatch_event_rule.profile_generator_lambda_event_rule.name
-# }
-
-
-
-
-##########################################################################
-
-
-
-
-
 resource "aws_sns_topic" "this" {
   name_prefix  = "${local.function_name}-Failure"
   display_name = "${local.function_name}-Failure"
