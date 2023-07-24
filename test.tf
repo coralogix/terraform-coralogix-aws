50cb6aa9945be92d39cee5c382a5fa463fb83099
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-07-24 10:03:35 +0300

    Delete test.tf

--- test.tf
+++
@@ -1,9 +0,0 @@
-module "resource-metadata" {
-  source = "./modules/resource-metadata"
-
-  coralogix_region    = "Custom"
-  custom_url = "ingress.staging.coralogix.net:443"
-  private_key         = "743e8f3a-3f0f-7daa-d7b5-19fac2492895"
-  ssm_enable          = "True"
-  layer_arn           = "arn:aws:lambda:us-east-1:035955823196:layer:coralogix-ssmlayer:21"
-}
