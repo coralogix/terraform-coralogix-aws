0a7654b94e73f95cec759d2724951eba6bedf2b7
guyrenny <guy.renny@coralogix.com>
2023-08-09 10:42:00 +0300

    fix test file

--- tests/lambda-secretLayer/lambda-secretLayer.tf
+++ tests/lambda-secretLayer/lambda-secretLayer.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "s3" {
-  source = "../../modules/secret-lambdaLayer"
+module "lambda-secretLayer" {
+  source = "../../modules/lambda-secretLayer"
 
 }
