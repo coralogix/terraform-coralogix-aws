18106e56eb9f644881bc21f4bc445fc621519caa
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-08-17 16:29:08 +0300

    Add support to US2 region and add option to use custom coralogix arn[CDS-552] (#78)
    * Add support to US2 region and add option to use custom coralogix arn
    
    * add coralogix_arn_mapping variable
    
    * add default value to coralogix_arn_mapping variable

--- examples/s3-archive/varibales.tf
+++ examples/s3-archive/varibales.tf
@@ -9,6 +9,25 @@ variable "bypass_valid_region" {
   default     = false
 }
 
+variable "custom_coralogix_arn" {
+  type        = string
+  description = "In case that you want to use a custom coralogix arn enter the aws account id that you want to use"
+  default     = ""
+}
+
+variable "coralogix_arn_mapping" {
+  type = map(string)
+  default = {
+    "eu-west-1"          = "625240141681"
+    "eu-north-1"         = "625240141681"
+    "ap-southeast-1"     = "625240141681"
+    "ap-south-1"         = "625240141681"
+    "us-east-2"          = "625240141681"
+    "us-west-2"          = "739076534691"
+    "default"            = "625240141681"
+  }
+}
+
 variable "logs_bucket_name" {
   type        = string
   description = "The name of the S3 bucket to create for the logs archive (Leave empty if not needed)"
