f9eab42453f80746018014971a5ce5bca9db7ddd
guyrenny <guy.renny@coralogix.com>
2023-08-20 12:53:27 +0300

    remove test.tf and update test file

--- test.tf
+++
@@ -1,7 +0,0 @@
-module "s3-archive" {
-  source = "./modules/provisioning/s3-archive"
-
-#   aws_region    = "eu-north-1"
-# bypass_valid_region = "us-east-1"
-  logs_bucket_name    = "gr-test326245"
-}
