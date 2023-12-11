688b360bf09a70b099557318f183b5283d9ca891
guyrenny <guy.renny@coralogix.com>
2023-12-11 18:10:27 +0200

    fix an issue with the suffix

--- modules/coralogix-aws-shipper/main.tf
+++ modules/coralogix-aws-shipper/main.tf
@@ -171,7 +171,7 @@ resource "aws_s3_bucket_notification" "lambda_notification" {
     lambda_function_arn = module.lambda.lambda_function_arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = var.s3_key_prefix != null || (var.integration_type != "CloudTrail" && var.integration_type != "VpcFlow") ? var.s3_key_prefix : "AWSLogs/"
-    filter_suffix       = var.integration_type == "S3" || var.s3_key_suffix != null ? var.s3_key_suffix : lookup(local.s3_suffix_map, var.integration_type)
+    filter_suffix       = (var.integration_type != "CloudTrail" && var.integration_type != "VpcFlow") || var.s3_key_suffix != null ? var.s3_key_suffix : lookup(local.s3_suffix_map, var.integration_type)
   }
 }
 
