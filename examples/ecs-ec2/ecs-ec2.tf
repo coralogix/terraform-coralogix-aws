7d71423c6f71e93ce79fd9e8997f3ac5ae89735b
Shaun Remekie <shaun.remekie@coralogix.com>
2024-10-08 18:42:20 +0200

    removed latest tag from example

--- examples/ecs-ec2/ecs-ec2.tf
+++ examples/ecs-ec2/ecs-ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 module "otel_ecs_ec2_coralogix" {
   source                   = "coralogix/aws/coralogix//modules/ecs-ec2"
   ecs_cluster_name         = "test-lab-cluster"
-  image_version            = "latest"
+  image_version            = "v0.3.1"
   memory                   = 256
   coralogix_region         = "EU1"
   custom_domain            = null
