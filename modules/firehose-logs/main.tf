475cc3b0e9916fe14a897c02b77fc2f1e1d5992f
K-Nakul-K <nakulkhargonkar@gmail.com>
2025-01-13 16:28:25 +0530

    Update main.tf

--- modules/firehose-logs/main.tf
+++ modules/firehose-logs/main.tf
@@ -114,6 +114,7 @@ resource "aws_s3_bucket_policy" "bucket_name_policy" {
   bucket = aws_s3_bucket.new_s3_bucket.id
   policy = data.aws_iam_policy_document.bucket_policy_doc.json
 }
+
 ################################################################################
 # Firehose Logs Stream
 ################################################################################
