86fc06b57df459867a116eee80a2ad7ac8b9b408
deri-krom <deri.satria@krom.id>
2025-02-28 14:46:37 +0700

    feat: add provider aliases

--- modules/coralogix-aws-shipper/versions.tf
+++ modules/coralogix-aws-shipper/versions.tf
@@ -5,6 +5,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.32.0"
+      configuration_aliases = [aws.bucket_s3]
     }
     random = {
       source  = "hashicorp/random"
