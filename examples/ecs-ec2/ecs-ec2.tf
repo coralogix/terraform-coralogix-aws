61b6969038b43f77523e51480bcbe4b59e5ddc63
dmenilo1 <david.menilo@coralogix.com>
2025-03-23 12:05:03 +0200

    CDS-1971 adding head sampling to terraform ECS-EC2 integration

--- examples/ecs-ec2/ecs-ec2.tf
+++ examples/ecs-ec2/ecs-ec2.tf
@@ -11,6 +11,4 @@ module "otel_ecs_ec2_coralogix" {
   default_application_name = "YOUR_APPLICATION_NAME"
   default_subsystem_name   = "YOUR_SUBSYSTEM_NAME"
   api_key                  = "1234567890_DUMMY_API_KEY"
-  otel_config_file         = "../../modules/ecs-ec2/otel_config.tftpl.yaml"
-  metrics                  = true
 }
