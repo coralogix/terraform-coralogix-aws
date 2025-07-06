7944aafb978f406fc7708801f8e5a39e9d2fc452
Yaniv Zlotnik <y.zlotnik@tricentis.com>
2025-07-06 17:01:44 +0300

    fix: server_side_encryption variable to kinesis_server_side_encryption
    Signed-off-by: Yaniv Zlotnik <y.zlotnik@tricentis.com>

--- modules/coralogix-aws-shipper/firehose.tf
+++ modules/coralogix-aws-shipper/firehose.tf
@@ -162,8 +162,8 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
   }
 
   server_side_encryption {
-    enabled  = var.server_side_encryption.enabled
-    key_type = var.server_side_encryption.key_type
-    key_arn  = var.server_side_encryption.key_arn
+    enabled  = var.kinesis_server_side_encryption.enabled 
+    key_type = var.kinesis_server_side_encryption.key_type
+    key_arn  = var.kinesis_server_side_encryption.key_arn
   }
 }
