30344bf39e28a4a1672df994f85184d98fc2201d
dennischristmann <dennis.christmann@proalpha.com>
2025-07-07 09:19:19 +0200

    Pin versions to NOT use aws provider v6 (#247)

--- modules/resource-metadata/versions.tf
+++ modules/resource-metadata/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.15.1"
+      version = ">= 4.15.1, < 6.0"
     }
     random = {
       source  = "hashicorp/random"
