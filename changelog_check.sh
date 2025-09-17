f3e66c3ce7fcc37cfbefd7105db3c843b4feddb4
vjofusha <vjosa.fusha@coralogix.com>
2025-09-17 15:58:46 +0200

    ci: fix changelog add cleanup

--- changelog_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Simply check if diff in changelog exists.
-git diff --exit-code --quiet origin/master... ./CHANGELOG.md
-if [ $? -ne 1 ]; then
-  echo "Please add a changelog entry in CHANGELOG.md or add 'skip changelog' label to your PR if this change does not require an entry".
-  exit 1
-fi
