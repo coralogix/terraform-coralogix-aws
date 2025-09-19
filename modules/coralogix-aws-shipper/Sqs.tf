c06ae667f53c59be65310907c5e41d15a317a79f
Vjosa <vjosa.fusha@gmail.com>
2025-09-19 09:07:31 +0200

    fix: notification sns and sqs (#266)

--- modules/coralogix-aws-shipper/Sqs.tf
+++ modules/coralogix-aws-shipper/Sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_notification" "sqs_notification" {
   depends_on = [module.lambda]
-  count      = var.sqs_name != null && (var.integration_type == "S3" || var.integration_type == "CloudTrail") ? 1 : 0
+  count      = var.sqs_name != null && (var.integration_type == "S3" || var.integration_type == "CloudTrail") && var.s3_notification != false ? 1 : 0
   bucket     = one(values(data.aws_s3_bucket.this)).bucket
   queue {
     queue_arn     = data.aws_sqs_queue.name[0].arn
