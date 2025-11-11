62ae69489621f20a417582d8868677d96902d9ca
vjofusha <vjosa.fusha@coralogix.com>
2025-11-11 17:25:35 +0100

    Upgrade AWS provider and min required Terraform version to 6.0 and 1.5.7

--- modules/resource-metadata/versions.tf
+++ modules/resource-metadata/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.1"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.15.1, < 6.0"
+      version = ">= 6.0"
     }
     random = {
       source  = "hashicorp/random"
