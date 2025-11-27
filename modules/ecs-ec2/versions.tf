3b84b1ff2c54984a933a4548bb724dc46eea5b2b
Misha <mikhail.chinkov@coralogix.com>
2025-11-27 13:01:57 +0100

    chore: bump aws provider to 6.x for all modules (#283)

--- modules/ecs-ec2/versions.tf
+++ modules/ecs-ec2/versions.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.24.0"
+      version = ">= 6.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.6.0"
+      version = ">= 3.0"
     }
   }
 }
